d0690c3fc9180fa3ad75b93032c723560802d87d
lynton-dev <61889203+lynton-dev@users.noreply.github.com>
2020-03-10 12:22:55 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
-function more() {
+function home() {
   var hero_main = document.getElementById("hero_main");
   var hero_footer = document.getElementById("hero_footer");
   var main_content = document.getElementById("main_content");
   
   if (window.getComputedStyle(hero_main).getPropertyValue("opacity") == 0) {
-    //Velocity(hero_main, "reverse", { duration: 1000 });
-    Velocity(hero_main, { opacity: 1 }, 1000);
-    Velocity(hero_footer, { opacity: 1 }, 1000);
-    Velocity(main_content, { opacity: 0 }, 1000);
-    hero_main.style.display = "block"
-    hero_footer.style.display = "block";
-    main_content.style.display = "none";
-  } else {
-    Velocity(hero_main, { opacity: 0 }, 1000);
-    Velocity(hero_footer, { opacity: 0 }, 1000);
-    Velocity(main_content, { opacity: 1 }, 1000);
-    hero_main.style.display = "none"
-    hero_footer.style.display = "none";
-    main_content.style.display = "block";
+    Velocity(main_content, { opacity: 0 }, { display: "none" }, 1000);
+    Velocity(hero_main, { opacity: 1 }, { display: "block", delay: 500, duration: 1000 });
+    Velocity(hero_footer, { opacity: 1 }, { display: "block", delay: 500, duration: 1000 });
+  }
+}
+
+function more() {
+  var hero_main = document.getElementById("hero_main");
+  var hero_footer = document.getElementById("hero_footer");
+  var main_content = document.getElementById("main_content");
+  
+  if (window.getComputedStyle(hero_main).getPropertyValue("opacity") != 0) {
+    Velocity(hero_main, { opacity: 0 }, { display: "none" }, 1000);
+    Velocity(hero_footer, { opacity: 0 }, { display: "none" }, 1000);
+    Velocity(main_content, { opacity: 1 }, { display: "block" , delay: 500, duration: 1000 });
   }
 }
