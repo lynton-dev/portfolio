c1b417bcd6b03e1ae19d679c8fc0091ead7d1b41
Lynton Schoeman <info@lynton.dev>
2020-03-15 18:32:11 -0600

    Changed stock images to real

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ function home() {
   var hero_footer = document.getElementById("hero_footer");
   var main_content = document.getElementById("main_content");
   var topnav_home = document.getElementById("topnav_home");
-  
+
+  Velocity("stop");
   Velocity(main_content, { opacity: 0 }, { display: "none" }, 1000);
   Velocity(topnav_home, { opacity: 0 }, { display: "none" }, 1000);
   Velocity(hero_main, { opacity: 1 }, { display: "block", delay: 500, duration: 1000 });
@@ -15,9 +16,18 @@ function more() {
   var hero_footer = document.getElementById("hero_footer");
   var main_content = document.getElementById("main_content");
   var topnav_home = document.getElementById("topnav_home");
-  
+
+  Velocity("stop");
   Velocity(hero_main, { opacity: 0 }, { display: "none" }, 1000);
   Velocity(hero_footer, { opacity: 0 }, { display: "none" }, 1000);
   Velocity(main_content, { opacity: 1 }, { display: "block" , delay: 500, duration: 1000 });
   Velocity(topnav_home, { opacity: 1 }, { display: "block" , delay: 500, duration: 1000 });
+}
+
+window.onhashchange = function() {
+  if (window.location.hash == "") {
+    home();
+  } else if (window.location.hash == "#my-works") {
+    more();
+  }
 }
