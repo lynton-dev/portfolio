26f870936cd1e9c3bb5998573f0ac3feba220138
lynton-dev <61889203+lynton-dev@users.noreply.github.com>
2020-05-03 14:46:46 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ function home() {
   var topnav_home = document.getElementById("topnav_home");
 
   Velocity("stop");
-  Velocity(main_content, { opacity: 0 }, { display: "none" }, 1000);
-  Velocity(topnav_home, { opacity: 0 }, { display: "none" }, 1000);
-  Velocity(hero_main, { opacity: 1 }, { display: "block", delay: 500, duration: 1000 });
-  Velocity(hero_footer, { opacity: 1 }, { display: "block", delay: 500, duration: 1000 });
+  Velocity(main_content, { opacity: 0, display: "none" }, 1000);
+  Velocity(topnav_home, { opacity: 0, display: "none" }, 1000);
+  Velocity(hero_main, { opacity: 1, display: "block" }, { delay: 500, duration: 1000 });
+  Velocity(hero_footer, { opacity: 1, display: "block" }, { delay: 500, duration: 1000 });
 }
 
 function more() {
@@ -18,10 +18,10 @@ function more() {
   var topnav_home = document.getElementById("topnav_home");
 
   Velocity("stop");
-  Velocity(hero_main, { opacity: 0 }, { display: "none" }, 1000);
-  Velocity(hero_footer, { opacity: 0 }, { display: "none" }, 1000);
-  Velocity(main_content, { opacity: 1 }, { display: "block" , delay: 500, duration: 1000 });
-  Velocity(topnav_home, { opacity: 1 }, { display: "block" , delay: 500, duration: 1000 });
+  Velocity(hero_main, { opacity: 0, display: "none" }, 1000);
+  Velocity(hero_footer, { opacity: 0, display: "none" }, 1000);
+  Velocity(main_content, { opacity: 1, display: "block" }, { delay: 500, duration: 1000 });
+  Velocity(topnav_home, { opacity: 1, display: "block" }, { delay: 500, duration: 1000 });
 }
 
 window.onhashchange = function() {
